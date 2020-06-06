6909d359a75ba2a312aa3f8e144935e732b38e85
Ivan Blažeka <ivblazek@vsite.hr>
2020-06-06 13:10:13 +0200

    Create txt and XML logger

---
+++ Template Method/TextLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Template_Method
+{
+    class TextLogger : Logger
+    {
+        public TextLogger(string path)
+        {
+            this.path = path;
+        }
+
+        protected override string WriteText(int severity, string message)
+        {
+            StringBuilder logMessage = new StringBuilder();
+            logMessage.Append("Log: ");
+            logMessage.Append(GetSeverity(severity));
+            logMessage.Append(", ");
+            logMessage.Append(DateTime.Now.ToString());
+            logMessage.Append(", ");
+            logMessage.Append(message);
+            return logMessage.ToString();
+        }
+    }
+}
---
+++ Template Method/XmlLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Method
+{
+    class XmlLogger : Logger
+    {
+        public XmlLogger (string path)
+        {
+            this.path = path;
+        }
+
+        protected override string WriteText(int severity, string message)
+        {
+            StringBuilder logMessage = new StringBuilder();
+            logMessage.Append("<log type=\"");
+            logMessage.Append(GetSeverity(severity));
+            logMessage.Append("\" datetime=\"");
+            logMessage.Append(DateTime.Now.ToString());
+            logMessage.Append("\">");
+            logMessage.Append(message);
+            logMessage.Append("</log>");
+            return logMessage.ToString();
+        }
+    }
+}
